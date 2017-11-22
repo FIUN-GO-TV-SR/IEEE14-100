d268d9f0197ba3cf2008f6eeecd5850309ebcd27
FIUN-GO-TV-SR <srriverar@unal.edu.co>
2017-11-22 15:13:55 -0500

    Add files via upload

--- MyMatlab1.m
+++ MyMatlab1.m
@@ -2,10 +2,12 @@
 % This script solves the problem and creates the 'solution1.txt' file
 %addpath('dependencies')
 global pfotemp
+%global solutionMPC_FIUN
 scenarioDir = pfotemp;
 rawFile = fullfile(scenarioDir, 'powersystem.raw');
 genFile = fullfile(scenarioDir, 'generator.csv');
 contingFile = fullfile(scenarioDir, 'contingency.csv');
 mpc = convert2mpc(rawFile,genFile);
 contingencies = loadContingencies(mpc,contingFile);
-[solutionMPC, ~] = solve_scenario(mpc, contingencies,1);
+[solutionMPC_FIUN, ~] = solve_scenario(mpc, contingencies,1);
+save('solution0_FIUN.mat','solutionMPC_FIUN')
--- MyMatlab2.m
+++ MyMatlab2.m
@@ -2,8 +2,12 @@
 % This script creates the 'solution2.txt' file
 %addpath('dependencies')
 global pfotemp
+%global solutionMPC_FIUN
+load('solution0_FIUN.mat')
 scenarioDir = pfotemp;
+rawFile = fullfile(scenarioDir, 'powersystem.raw');
+genFile = fullfile(scenarioDir, 'generator.csv');
 contingFile = fullfile(scenarioDir, 'contingency.csv');
 mpc = convert2mpc(rawFile,genFile);
 contingencies = loadContingencies(mpc,contingFile);
-create_solution2(solutionMPC, contingencies,1);
+create_solution2(solutionMPC_FIUN, contingencies,1);
